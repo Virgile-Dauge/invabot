2f93fe37cdacea2c2b9b3a762f04bf24c90ff3a8
Virgile <virgile.dauge@pm.me>
2021-12-21 00:46:26 +0100

    ajout bases documentation des rôles

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ def dtag(user):
 import pandas as pd
 def get_roster(config):
     url = f"{config['gdoc']['url']}gviz/tq?tqx=out:csv&sheet={config['gdoc']['page_roster']}"
-    print(pd.read_csv(url).iloc[1:, 0:6].dropna().set_index('dtag'))
     return pd.read_csv(url).iloc[1:, 0:6].dropna().set_index('dtag')
 
 def get_strat(config):
@@ -36,8 +35,11 @@ async def on_ready():
 
 @bot.command()
 async def inva(ctx, arg):
+    """ Génére un tableau d'inscription dans le channel
+    """
     embed = discord.Embed.from_dict(config["embeds"]["panneau"])
     embed.title += f' {arg}'
+    embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
 
     msg = await ctx.channel.send(embed=embed)
     await msg.add_reaction('☑')
@@ -46,6 +48,42 @@ async def inva(ctx, arg):
     msgs[author_id] = msg.id
     global msgs_ids
     msgs_ids += [msg.id]
+
+@bot.command()
+async def comp(ctx):
+    """ Génére une composition d'armée
+    """
+    author_dtag = dtag(ctx.author)
+    channel = ctx.channel
+
+    # On s'assure que l'utilisateur qui demande le calcul ait déjà fait la commande principale
+    if author_dtag not in msgs:
+        await channel.send(f"Pas de messages trouvés pour {author_dtag}. Commencez par utiliser la commande :\n $inva nomVille")
+        return
+
+    # Récupération du message avec les réacs
+    msg_id = msgs[author_dtag]
+    main_msg = await channel.fetch_message(msg_id)
+
+    # Récupération de la liste des users
+    selected = await main_msg.reactions[0].users().flatten()
+    selected_tags = [dtag(u) for u in selected][1:]
+    selected_snow = {t: u.id for t, u in zip(selected_tags, selected)}
+
+    # Récupération des donénes du Gdoc roster
+    roster = get_roster(config)
+
+    # Joueurs séléctionnés n'ayant pas rempli le Gdoc
+    not_registered = [u for u in selected_tags if u not in roster.index]
+
+    # Filtrage du roster avec les joueurs séléctionnés
+    roster = roster.filter(items=selected_tags, axis=0)
+
+    # Récupération de la strat
+    strat = get_strat(config)
+    print(roster)
+    print(strat)
+    await channel.send(roster)
 
 @bot.event
 async def on_reaction_add(reaction, user):
@@ -67,35 +105,5 @@ async def on_reaction_add(reaction, user):
      else:
          # Plus tard, ajouter l'utilisateur aux tryhard s'il n'y est pas déjà
          pass
-
-@bot.command()
-async def comp(ctx):
-    author_dtag = dtag(ctx.author)
-    channel = ctx.channel
-
-    # On s'assure que l'utilisateur qui demande le calcul ait déjà fait la commande principale
-    if author_dtag not in msgs:
-        await channel.send(f"Pas de messages trouvés pour {author_dtag}. Commencez par utiliser la commande :\n $inva nomVille")
-        return
-
-    # Récupération du message avec les réacs
-    msg_id = msgs[author_dtag]
-    main_msg = await channel.fetch_message(msg_id)
-
-    # Récupération de la liste des users
-    selected = await main_msg.reactions[0].users().flatten()
-    selected_tags = [dtag(u) for u in selected][1:]
-    selected_snow = {t: u.id for t, u in zip(selected_tags, selected)}
-    await channel.send(f'Joueurs séléctionés : {selected}')
-
-    # Récupération des donénes du Gdoc roster
-    df = get_roster(config)
-
-    # Joueurs séléctionnés n'ayant pas rempli le Gdoc
-    not_registered = [u for u in selected_tags if u not in df.index]
-
-    # Filtrage du gdoc avec les joueurs séléctionnés
-    df = df.filter(items=selected, axis=0)
-    print(selected, not_registered, df)
 
 bot.run(token)
